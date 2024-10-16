124d2d7bef5398e71f89d6f35ad65f95395739c3
hollaz <osasha230905@gmail.com>
2024-10-16 14:35:55 +0300

    пофиксил кнопки под гугл хром, добавил палец

--- src/components/pult.jsx
+++ src/components/pult.jsx
@@ -7,6 +7,7 @@ import keytwo from '../assets/key2.png'
 import krestovina from '../assets/krestovinabtn.png'
 import minus from '../assets/minus.png'
 import plus from '../assets/plus.png'
+import palec from '../assets/palec.png'
 import '../App.css'
 
 function Pult() {
@@ -14,18 +15,24 @@ function Pult() {
 
     return (
         <>
-            <div className="pult-container absolute left-[1444px] top-[425px] rotate-[-9deg]">
-                <img className={'btnstart '} src={btnstart} alt="btnstart"/>
-                <img className={'bigbtn relative top-[100px] left-[20px]'} src={bigbtn} alt="bigbtn"/>
-                <img className={'home relative top-[150px] left-[27px]'} src={home} alt="home"/>
-                <img className={'keyone relative top-[252px] left-[20px]'} src={keyone} alt="keyone"/>
-                <img className={'keytwo relative top-[267px] left-[20px]'} src={keytwo} alt="keytwo"/>
-                <img className={'krestovina relative top-[-85px] left-[12px] rotate-[2deg]'} src={krestovina} alt="krestovina"/>
-                <img className={'krestovina relative top-[-105px] left-[49px] rotate-[2deg]'} src={krestovina} alt="krestovina"/>
-                <img className={'krestovina relative top-[-109px] left-[30px] rotate-[90deg]'} src={krestovina} alt="krestovina"/>
-                <img className={'krestovina relative top-[-167px] left-[31px] rotate-[-90deg]'} src={krestovina} alt="krestovina"/>
-                <img className={'minus relative top-[-14px] left-[-5px]'} src={minus} alt="minus"/>
-                <img className={'plus relative top-[-34px] left-[61px]'} src={plus} alt="plus"/>
+            <div className="pult-container transform translate-x-[1450px] translate-y-[413px] w-[140px] rotate-[-9deg]">
+                <img className={'palec absolute transform translate-x-[41px] translate-y-[105px]  z-[2]'} src={palec} alt="plus"/>
+                <img className={'btnstart cursor-pointer'} src={btnstart} alt="btnstart"/>
+
+                <img className={'bigbtn relative top-[98px] left-[19px] cursor-pointer z-[1]'} src={bigbtn} alt="bigbtn"/>
+                <img className={'home relative top-[149px] left-[27px] cursor-pointer'} src={home} alt="home"/>
+                <img className={'keyone relative top-[250px] left-[20px] cursor-pointer'} src={keyone} alt="keyone"/>
+                <img className={'keytwo relative top-[263px] left-[20px] cursor-pointer'} src={keytwo} alt="keytwo"/>
+                <img className={'krestovina relative top-[-85px] left-[12px] rotate-[2deg] cursor-pointer'}
+                     src={krestovina} alt="krestovina"/>
+                <img className={'krestovina relative top-[-105px] left-[49px] rotate-[2deg] cursor-pointer'}
+                     src={krestovina} alt="krestovina"/>
+                <img className={'krestovina relative top-[-109px] left-[30px] rotate-[90deg] cursor-pointer'}
+                     src={krestovina} alt="krestovina"/>
+                <img className={'krestovina relative top-[-167px] left-[31px] rotate-[-90deg] cursor-pointer'}
+                     src={krestovina} alt="krestovina"/>
+                <img className={'minus relative top-[-16px] left-[-6px] cursor-pointer'} src={minus} alt="minus"/>
+                <img className={'plus relative top-[-35px] left-[61px] cursor-pointer'} src={plus} alt="plus"/>
             </div>
 
 
