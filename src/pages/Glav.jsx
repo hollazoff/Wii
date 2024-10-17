677fb402cb87165c7e461949c2e7572e7e2531d9
hollaz <osasha230905@gmail.com>
2024-10-17 17:53:40 +0300

    fix x2

--- src/pages/Glav.jsx
+++ src/pages/Glav.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client';
 import Pult from '../components/Pult.jsx';
 import ScreenOff from '../components/ScreenOff.jsx';
 import Screenonstart from '../components/ScreeonStart.jsx';
-
+import ScreenShop from '../components/ScreenShop.jsx';
+import ScreenGame from "../components/ScreenGame.jsx";
+import ScreenAchivment from "../components/ScreenAchivment.jsx";
 import '../index.css';
+
 
 
 createRoot(document.getElementById('root')).render(
